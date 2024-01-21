0bc8d161943cbb7d08ce72addce2fb142f21383d
Christian Cook <christiancook52@gmail.com>
2024-01-20 22:10:25 -0500

    Update Building.cs

--- Resources/Building.cs
+++ Resources/Building.cs
@@ -6,10 +6,20 @@ public partial class Building : Resource
 	[Export] public int maxTurnsOfHealth = 1;
 	[Export] public int buildCost;
 	[Export] public int repairCost;
+	[Export] public BuildingType type;
 }
 
 public enum ResourceType {
 	WOOD,
 	ELECTRICITY,
 	STEEL
+}
+
+public enum BuildingType {
+	WALL,
+	LAZER,
+	LANDMINE,
+
+	LUMBER_MILL,
+	POWER_PLANT
 }
