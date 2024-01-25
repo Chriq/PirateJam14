f1b2cf781de95cca66d2fb476d4e02a3e7661b8f
Jack Chandler <>
2024-01-24 22:34:57 -0500

    Menu Popup

--- Global/SceneChange.cs
+++ Global/SceneChange.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class SceneChange : Node
 {
+	Node sc = null;
+	
 	public void LoadMenu()
 	{
 		GetTree().ChangeSceneToFile("res://Scenes/MainMenu.tscn");
@@ -13,8 +15,33 @@ public partial class SceneChange : Node
 		GetTree().ChangeSceneToFile("res://Scenes/Game.tscn");
 	}
 	
+	public void PopupInstructions()
+	{
+		if (sc != null)
+			ClosePopup();
+			
+		sc = GD.Load<PackedScene>("res://Scenes/PopupMenu/Instructions.tscn").Instantiate();
+		GD.Print(sc);
+		GetTree().Root.AddChild(sc);
+	}
+	public void PopupCredits()
+	{
+		if (sc != null)
+			ClosePopup();
+			
+		sc = GD.Load<PackedScene>("res://Scenes/PopupMenu/Credits.tscn").Instantiate();
+		GD.Print(sc);
+		GetTree().Root.AddChild(sc);
+	}
+	public void ClosePopup()
+	{
+		sc.QueueFree();
+		sc = null;
+	}
+	
 	public void ExitGame()
 	{
 		GetTree().Quit();
 	}
 }
+
